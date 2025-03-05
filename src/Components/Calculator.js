82bbb83308859588e9a8345767926dddeedc2033
Simon Aust <simon.aust@googlemail.com>
2025-03-05 14:21:47 +0000

    FCC tests passed

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,205 +1,262 @@
 import CalculatorDisplay from "./CalculatorDisplay";
 import CalculatorInterface from "./CalculatorInterface";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function Calculator() {
+  const [input, setInput] = useState("");
+  const [expression, setExpression] = useState([]);
 
-    const [input, setInput] = useState("");
-    const [expression, setExpression] = useState([]);
+  const [history, setHistory] = useState(["1 + 2 = 3"]);
 
-    const [history, setHistory] = useState(["1 + 2 = 3"]);
+  useEffect(() => {
+    document.addEventListener("keydown", keyPressHandler);
+    return () => document.removeEventListener("keydown", keyPressHandler);
+  });
 
-    useEffect(() => {
-        document.addEventListener("keydown", keyPressHandler);
-        return () => document.removeEventListener("keydown", keyPressHandler)
-    });
+  useEffect(() => console.debug(`term: ${input}`, [input]));
+  useEffect(() => {
+    console.debug(`expression: ${expression}`);
+    if (expression[expression.length - 1] === "=") {
+      calculateResult();
+    }
+  }, [expression]);
 
-    useEffect(() => console.debug(`term: ${input}`, [input]));
-    useEffect(() => {
-        console.debug(`expression: ${expression}`);
-        if (expression[expression.length -1] === "=") {
-            calculateResult();
+  /* Handle keyboard input */
+  function keyPressHandler(e) {
+    if (e.repeat) {
+      return;
+    }
+    console.log(e.key);
+    switch (e.key) {
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "0":
+      case ".":
+        if (inputContainsDecimal() && e.key === ".") break;
+        setInput(e.key);
+        addToExpression(input, e.key);
+        break;
+      case "/":
+      case "*":
+      case "+":
+        addToExpression(input, e.key);
+        // setTerm("");
+        break;
+      case "-":
+        // add negative operand to the input
+        break;
+      case "Enter":
+        addToExpression(input, "=");
+        // calculateResult();
+        break;
+      case "Escape": // allClear
+        allClear();
+        break;
+      case "Backspace": // remove last operation
+        break;
+      default:
+        console.log(e.key);
+    }
+  }
+
+  function handleInput(e) {
+    console.debug("Input: ", e.target.id);
+    switch (e.target.id) {
+      case "subtract":
+      case "add":
+      case "multiply":
+      case "divide":
+        addToExpression(null, e.target.value);
+        setInput(e.target.value);
+        break;
+      case "clear":
+        allClear();
+        break;
+      case "equals":
+        addToExpression(null, "=");
+        // calculateResult();
+        break;
+      case "decimal":
+        /* Check if there is already a decimal point */
+        if (inputContainsDecimal() && e.target.value === ".") {
+          break;
+        } else {
+          addToExpression(e.target.value);
+          setInput((prevState) => prevState + e.target.value);
         }
-    }, [expression]);
-
-    function keyPressHandler(e) {
-        if (e.repeat) {
-            return
+        /* Prefix a zero before a decimal fraction */
+        if (e.target.value === "." && input.length === 0) {
+          setInput((prevState) => prevState + "0" + e.target.value);
+          break;
         }
-        console.log(e.key)
-        switch (e.key) {
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-            case "0":
-            case ".":
-                if (inputContainsDecimal() && e.key === ".") break;
-                setInput(prevState => prevState + e.key);
-                break;
-            case "/":
-            case "*":
-            case "+":
-                addToExpression(input, e.key);
-                // setTerm("");
-                break;
-            case "-":
-                // add negative operand to the input
-                break;
-            case "Enter":
-                addToExpression(input, "=");
-                // calculateResult();
-                break;
-            case "Escape": // allClear
-                allClear();
-                break;
-            case "Backspace": // remove last operation
-                break;
-            default:
-                console.log(e.key);
-        }
-    }
+        break;
+      /* Input is a number */
+      default:
+        console.log("Case: default");
 
+        /* If input has an operator */
+        if (/[+/*-]/.test(input)) {
+          console.log("In the test default");
 
-    function handleInput(e) {
-
-        switch (e.target.id) {
-            case "subtract":
-                // setInput();
-                console.debug(`in subtract: ${lastInputIsAnOperator()}`);
-                if (lastInputIsAnOperator()) {
-                    // setInput(prevState => "-" + prevState);
-                    // break;
-                    if (input.match(/^-\d*$/)) {
-                        // The input is a negative number
-                        setInput(prevState => prevState.replace("-", ""));
-                        break;
-                    } else {
-                        setInput(prevState => "-" + prevState);
-                        break;
-                    }
-
-                }
-            case "add":
-            case "multiply":
-            case "divide":
-                addToExpression(input, e.target.value);
-                // reset input
-                // setTerm("");
-                break;
-            case "clear":
-                allClear();
-                break;
-            case "equals":
-                addToExpression(input, "=");
-                // calculateResult();
-                break;
-            default:
-                if (inputContainsDecimal() && e.target.value === ".") break;
-                if (e.target.value === "." && input.length === 0) {
-                    setInput(prevState => prevState + "0" + e.target.value);
-                    break;
-                }
-                if (input.length === 0 && e.target.value === "0") break;
-                setInput(prevState => prevState + e.target.value);
-                console.info(`Added to term: ${e.target.value}. Term = ${input}`);
-                break;
-        }
-    }
-
-    const inputContainsDecimal = () => input.includes(".");
-
-    // const lastOperatorIsMinus = () => expression[expression.length - 1] === "-";
-    const lastInputIsAnOperator = () => expression[expression.length - 1].match(/[-+*/]/); //todo
-    /**
-     * Add the arguments to the expression. If @operand is falsy, change the last @operator to the
-     * current argument.
-     * @param operand {String=} Optional, the operand to add to the expression, preceding the operator. A single
-     * character matching one of [0,1,2,3,4,5,6,7,8,9,.]
-     * @param operator {String} Required. One of [+-/*=]
-     * */
-    function addToExpression(operand, operator) {
-            if (operand && !operand.match(/[\d.]/)) {
-                console.error(`Operand not valid: ${operand}`);
-                return;
-            }
-            if (!operator.match(/[/*-=+]/)) {
-                console.error(`Operator not valid: ${operator}`);
-                return;
-            }
-            if (!operand && operator) {
-
-                const updatedExpression = expression.map((element, index) => {
-                    if (index === expression.length - 1) {
-                        console.info(`Replaced operator "${expression[expression.length - 1]}" with new operator "${operator}"`);
-                        return operator;
-                    } else {
-                        return element;
-                    }
-                })
-                setExpression(updatedExpression);
-                return;
-            }
-
-            setExpression(prevState => [...prevState, operand, operator]);
-            console.info(`Added operand "${operand}" & operator ${operator} to expression. Expression now: ${expression}`);
-
-        setInput("");
-    }
-    // todo first input of 'minus' is a negative number, second click replacing last operator
-
-    /**
-     * Clears the expression and the input.
-     * */
-    function allClear() {
-        console.info("Cleared expression");
-        setExpression([]);
-        setInput("");
-    }
-
-    /**
-     * Clears only the input.
-     * */
-    function clear() {
-        // TODO
-    }
-
-    const operations = {
-        "+": (a, b) => a + b,
-        "/": (a, b) => a / b,
-        "-": (a, b) => a - b,
-        "*": (a, b) => a * b
-    }
-
-
-    function calculateResult() {
-        console.info("Calculating result");
-        let tempResult = expression[0]; // set to initial value
-        console.log(`tempResult: ${tempResult}`);
-        for (let i = 2; i < expression.length - 1; i += 2) {
-            tempResult = operations[expression[i - 1]](+tempResult, +expression[i]);
-            console.log(`tempResult: ${tempResult}`);
+          setInput(e.target.value);
+          addToExpression(e.target.value);
+          break;
         }
 
-        setHistory(prevState => [...prevState,
-            expression
-                .join(" ")
-                .concat(` ${tempResult}`)]);
-        setExpression([]); // allClear?
-        setInput(tempResult + "");
+        /* Check if adding zero to nothing? */
+        if (input.length === 0 && e.target.value === "0") break;
+
+        /* Set input and expression values */
+        addToExpression(e.target.value);
+        setInput((prevState) => prevState + e.target.value);
+        console.info(`Added to term: ${e.target.value}. Term = ${input}`);
+        break;
+    }
+  }
+
+  /* Boolean condition tests */
+  const inputContainsDecimal = () => input.includes(".");
+  const lastInputIsAnOperator = () => /^.*[+/*-]+$/.test(expression.join(""));
+
+  /**
+   * Add the arguments to the expression. If @operand is falsy, change the last @operator to the
+   * current argument.
+   * This is essentially simulating an overloaded method.
+   * @param operand {String=} Optional, the operand to add to the expression, preceding the operator. A single
+   * character matching one of [0,1,2,3,4,5,6,7,8,9,.]
+   * @param operator {String=} Optional. One of [+-/*=]
+   * */
+  function addToExpression(operand, operator) {
+    /* Operand is not a number 0-9 or a decimal point */
+    if (operand && !operand.match(/[\d.]/)) {
+      console.error(`Operand not valid: ${operand}`);
+      return;
+    }
+    /* Operator not valid */
+    if (operator && !operator.match(/[/*-=+]/)) {
+      console.error(`Operator not valid: ${operator}`);
+      return;
+    }
+    /* if expression already has a negative operator, remove it */
+    if (!operand && operator && /^.*[+/*-]-$/.test(expression.join(""))) {
+      console.debug("Removing negative operator");
+      let updatedExpression = expression.slice(0, -2);
+      updatedExpression.push(operator);
+      console.log(`UpdatedExpression: ${updatedExpression}`);
+      setExpression(updatedExpression);
+      return;
     }
 
-    return (
-        <>
-            <CalculatorDisplay history={history} expression={expression} input={input}/>
-            <CalculatorInterface handleInput={handleInput}/>
-        </>
-    )
+    /* If only an operator is supplied as an argument, and if the last part of the expression is an operator */
+    if (!operand && operator && lastInputIsAnOperator()) {
+      /* Operator argument is a minus, next input will be a negative number */
+      if (operator === "-" && /^.*[*-/+]?$/.test(expression.join(""))) {
+        console.debug("Adding negative operator");
+        setExpression((prevState) => [...prevState, operator]);
+        return;
+      }
+
+      /*  switch the current operator in the expression to the supplied argument  */
+      const updatedExpression = expression.map((element, index) => {
+        if (index === expression.length - 1) {
+          console.info(
+            `Replaced operator "${
+              expression[expression.length - 1]
+            }" with new operator "${operator}"`
+          );
+          return operator;
+        } else {
+          return element;
+        }
+      });
+      setExpression(updatedExpression);
+      return;
+    }
+
+    /* Only an operand is supplied as an argument, upadate the expression */
+    if (operand && !operator) {
+      console.log(`Updading the expression with the input ${operand}`);
+      setExpression((prevState) => [...prevState, operand]);
+      return;
+    }
+
+    /* Only an operator is supplied as an argument */
+    if (!operand && operator) {
+      setExpression((prevState) => [...prevState, operator]);
+    }
+
+    setInput("");
+  }
+
+  /**
+   * Clears the expression and the input.
+   * */
+  function allClear() {
+    console.info("Cleared expression");
+    setExpression([]);
+    setInput("");
+  }
+
+  const operations = {
+    "+": (a, b) => a + b,
+    "/": (a, b) => a / b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+  };
+
+  function calculateResult() {
+    console.info("Calculating result");
+    try {
+      const expressionString = expression.join("").replace("=", "");
+      // Get terms
+      const terms = expressionString.split(/(?<![+/*-])[+/*-]/);
+      const operators = expressionString.match(/(?<=\d+?)[+*/-](?=-?\d+)/g);
+      console.debug("Terms: ", terms);
+      console.debug("Operators: ", operators);
+
+      let result = 0;
+      let operator = "";
+      if (terms.length > 0) {
+        result = terms[0];
+        for (let i = 1; i < terms.length; i++) {
+          // Get the operator
+          operator = operators[i - 1];
+          const statement = `${result} ${operator} ${terms[i]}`;
+          result = operations[operator](+result, +terms[i]);
+
+          console.log(`Calculation: ${statement} = ${result}`);
+        }
+      }
+
+      // Clean up
+      setHistory((prevState) => [
+        ...prevState,
+        expression.join(" ").concat(` ${result}`),
+      ]);
+      setExpression([result]);
+      setInput(result + "");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <CalculatorDisplay
+        history={history}
+        expression={expression}
+        input={input}
+      />
+      <CalculatorInterface handleInput={handleInput} />
+    </>
+  );
 }
 
 export default Calculator;
